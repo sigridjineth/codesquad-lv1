1e922b65f242c90075bfde025d36716ca51c30d0
jypthemiracle <hophfg@yahoo.co.kr>
2019-11-15 19:37:04 +0900

    4단계 기능구현 수정
    main() 함수 기능 조정

--- question_3/random.js
+++ question_3/random.js
@@ -1,6 +1,9 @@
 //get input
 var input = prompt('10개의 단어를 입력하세요. 단어 별 구분은 comma로 합니다.');
 
+//mark progress
+var str = "";
+
 //get HTML elements
 var word1 = document.getElementById('word1');
 var word2 = document.getElementById('word2');
@@ -16,7 +19,7 @@ var game = {
 game.words = input.split(',');
 
 //choose one word from input, and put characters into HTML
-game.choose = function(){
+game.choose = function() {
     var index = Math.floor(Math.random() * this.words.length);
     this.answer = this.words[index];
     this.characters = this.answer.split('');
@@ -24,8 +27,8 @@ game.choose = function(){
 }
 
 //align button by putting characters into HTML
-game.alignbutton = function(){
-    for (i=0; i<this.characters.length; i++){
+game.alignbutton = function() {
+    for (i = 0; i < this.characters.length; i++) {
         var newbutton = document.createElement('button');
         newbutton.innerHTML = this.characters[i];
         word2.appendChild(newbutton);
@@ -33,27 +36,27 @@ game.alignbutton = function(){
     }
 };
 
-game.checkValid = function(){
+game.checkValid = function() {
     return this.answer === this.characters.join("");
 };
 
-game.updateValid = function(){
+game.updateValid = function() {
     if (this.checkValid()) {
         check.innerHTML = "일치합니다."
     } else {
-        check.innerHTML = "일치하지 않습니다." 
+        check.innerHTML = "일치하지 않습니다."
     }
 };
 
-game.init = function(){
+game.init = function() {
     this.choose();
     this.alignbutton();
     this.updateValid();
 }
 
-game.updatebutton = function(){
+game.updatebutton = function() {
     word2.innerHTML = '';
-    for (i = 0; i<this.characters.length; i++){
+    for (i = 0; i < this.characters.length; i++) {
         var newbutton = document.createElement('button');
         newbutton.innerHTML = this.characters[i];
         word2.appendChild(newbutton);
@@ -62,9 +65,9 @@ game.updatebutton = function(){
     }
 };
 
-game.swap = function(){
+game.swap = function() {
     var temp = [];
-    while (this.characters.length != 0){
+    while (this.characters.length != 0) {
         var s = this.characters.pop();
         temp.push(s);
     }
@@ -73,23 +76,23 @@ game.swap = function(){
     game.updateValid();
 };
 
-game.lshift = function(){
+game.lshift = function() {
     var s = this.characters.shift();
     this.characters.push(s);
     game.updatebutton();
     game.updateValid();
 }
 
-game.rshift = function(){
+game.rshift = function() {
     var s = this.characters.pop();
     this.characters.unshift(s);
     game.updatebutton();
     game.updateValid();
 }
 
-game.shuffle = function(){
+game.shuffle = function() {
     var random = Math.floor(Math.random() * 10);
-    if (random == 5){
+    if (random == 5) {
         this.choose();
     } else if (random < 4) {
         this.lshift();
@@ -98,54 +101,51 @@ game.shuffle = function(){
     }
 };
 
-game.play = function(){
-    if (this.checkValid){
-        game.init();
+game.play = function() {
+    if (this.checkValid()) {
+        game.choose();
         game.shuffle();
-
-        var str = "";
-        str += game.current;
         word3.innerHTML = str;
 
-        for (i = 0; i < game.current; i++){
-            var str = "";
+        for (i = 0; i < game.current; i++) {
             str += "O"
-        }
+        };
         word3.innerHTML = str;
         game.current += 1;
-    }
+    };
 
-    if (this.current == this.maxPlay){
+    if (game.current == game.maxPlay) {
         alert('Thanks for your playing!');
-        break;
-    }
+    };
 };
 
 
 //main function
-var lshift = function(){
+var lshift = function() {
     game.lshift();
+    game.play();
 };
 
-var rshift = function(){
+var rshift = function() {
     game.rshift();
+    game.play();
 };
 
-var swap = function(){
+var swap = function() {
     game.swap();
+    game.play();
 };
 
-var shuffle = function(){
+var shuffle = function() {
     game.shuffle();
+    game.play();
 };
 
 
 //main function
-function main(){
+function main() {
     game.init();
-    while (game.current != game.maxPlay){
-        game.play();
-    }
+    game.shuffle();
+    game.play();
 };
-
 main();
